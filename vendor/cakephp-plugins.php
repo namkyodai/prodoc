942ba125ea77fc6949d0aa6ab8a5b7f6d843b9bc
Nam Le <namkyodai@gmail.com>
2018-08-05 19:23:16 +0800

    first commit

---
+++ vendor/cakephp-plugins.php
@@ -0,0 +1,14 @@
+<?php
+$baseDir = dirname(dirname(__FILE__));
+return [
+    'plugins' => [
+        'Bake' => $baseDir . '/vendor/cakephp/bake/',
+        'CakeDC/Auth' => $baseDir . '/vendor/cakedc/auth/',
+        'CakeDC/Users' => $baseDir . '/vendor/cakedc/users/',
+        'DebugKit' => $baseDir . '/vendor/cakephp/debug_kit/',
+        'Josegonzalez/Upload' => $baseDir . '/vendor/josegonzalez/cakephp-upload/',
+        'Migrations' => $baseDir . '/vendor/cakephp/migrations/',
+        'Proffer' => $baseDir . '/vendor/davidyell/proffer/',
+        'WyriHaximus/TwigView' => $baseDir . '/vendor/wyrihaximus/twig-view/'
+    ]
+];
